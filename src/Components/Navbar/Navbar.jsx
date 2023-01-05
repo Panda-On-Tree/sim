08b49214e894d5d7f2373910f93c8b5716ad7282
Aniket Khatri <aksonipat2000@gmail.com>
2023-01-05 10:35:11 +0530

    scanning

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,86 +1,88 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { SlButton, SlDialog, SlDivider, SlDropdown, SlIcon, SlMenu, SlMenuItem } from '@shoelace-style/shoelace/dist/react/index';
-import logo from './logo2.png'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { baseurl } from '../../api/apiConfig';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { SlButton, SlDropdown, SlMenu, SlMenuItem } from "@shoelace-style/shoelace/dist/react/index";
+import logo from "./logo2.png";
+import { useNavigate } from "react-router-dom";
 function Navbar() {
+	let navigate = useNavigate();
 
-    let navigate = useNavigate()
+	return (
+		<div className="navbar">
+			<div className="navbar-main">
+				<nav className="navbar-inner">
+					<div className="navbar-logo">
+						<img src={logo} className="navbar-logo" alt="" />
+					</div>
+					<h2 className="navbar-head">Serial Number Inventory</h2>
+					<div className="nav-items-main">
+						<SlButton
+							className="nav-item-button nav-item-alternate"
+							caret
+							onClick={() => {
+								navigate("/filter-serial");
+							}}>
+							Home
+						</SlButton>
 
+						<SlDropdown distance={5} className="nav-item">
+							<SlButton className="nav-item-button" slot="trigger" caret>
+								Scanning
+							</SlButton>
+							<SlMenu>
+								<SlMenuItem
+									onclick={() => {
+										navigate("/scanning");
+									}}>
+									Scan
+								</SlMenuItem>
+							</SlMenu>
+						</SlDropdown>
+						<SlDropdown distance={5} className="nav-item">
+							<SlButton className="nav-item-button" slot="trigger" caret>
+								Serial
+							</SlButton>
+							<SlMenu>
+								<SlMenuItem
+									onclick={() => {
+										navigate("/serial");
+									}}>
+									Serial Generation
+								</SlMenuItem>
+								<SlMenuItem
+									onclick={() => {
+										navigate("/filter-serial");
+									}}>
+									Serial View
+								</SlMenuItem>
+								<SlMenuItem
+									onclick={() => {
+										navigate("/combo-gen");
+									}}>
+									Combo Generation
+								</SlMenuItem>
+							</SlMenu>
+						</SlDropdown>
 
-
-  return (
-   <div className='navbar'>
- <div className='navbar-main'>
-        <nav className='navbar-inner'>
-            <div className='navbar-logo'>
-                <img src={logo}  className='navbar-logo' alt="" />  
-            </div>
-            <h2 className='navbar-head'>Serial Number Inventory</h2>
-            <div className='nav-items-main'>
-            <SlButton className='nav-item-button nav-item-alternate' caret  onClick={()=>{
-                     navigate("/filter-serial")
-                }}>
-                    Home
-                </SlButton>
-               
-                <SlDropdown distance={5} className="nav-item">
-                <SlButton className='nav-item-button' slot="trigger" caret>
-                    Scanning
-                </SlButton>
-                {/* <SlMenu>
-                <SlMenuItem onclick={()=>{
-                   
-                }}>Scan !</SlMenuItem>
-                <SlMenuItem onclick={()=>{
-                   
-                }}>Scan 2</SlMenuItem>
-              
-                
-                </SlMenu> */}
-            </SlDropdown> 
-                <SlDropdown distance={5} className="nav-item">
-                <SlButton className='nav-item-button' slot="trigger" caret>
-                    Serial
-                </SlButton>
-                <SlMenu>
-                <SlMenuItem onclick={()=>{
-                   navigate("/serial")
-                }}>Serial Generation</SlMenuItem>
-                <SlMenuItem onclick={()=>{
-                   navigate("/filter-serial")
-                }}>Serial View</SlMenuItem>
-                <SlMenuItem onclick={()=>{
-                   navigate("/combo-gen")
-                }}>Combo Generation</SlMenuItem>
-              
-                
-                </SlMenu>
-            </SlDropdown> 
-           
-            <SlDropdown distance={5} className="nav-item">
-                <SlButton className='nav-item-button' slot="trigger" caret>
-                    Account
-                </SlButton>
-                <SlMenu>
-                <SlMenuItem onclick={()=>{
-                   
-                }}>{localStorage.getItem("fullname")}</SlMenuItem>
-                <SlMenuItem onclick={()=>{
-                     localStorage.clear()
-                     navigate("/login")
-                }}>LogOut</SlMenuItem>
-              
-                
-                </SlMenu>
-            </SlDropdown> 
-            </div>
-        </nav>
-    </div>
-   </div>
-  )
+						<SlDropdown distance={5} className="nav-item">
+							<SlButton className="nav-item-button" slot="trigger" caret>
+								Account
+							</SlButton>
+							<SlMenu>
+								<SlMenuItem onclick={() => {}}>{localStorage.getItem("fullname")}</SlMenuItem>
+								<SlMenuItem
+									onclick={() => {
+										localStorage.clear();
+										navigate("/login");
+									}}>
+									LogOut
+								</SlMenuItem>
+							</SlMenu>
+						</SlDropdown>
+					</div>
+				</nav>
+			</div>
+		</div>
+	);
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/Pages/Scanning/Scanning.jsx
@@ -0,0 +1,217 @@
+import { Autocomplete, Button, Paper, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { toast } from "react-toastify";
+import { baseurl } from "../../api/apiConfig";
+import "./Scanning.css";
+function Scanning() {
+	const [productList, setProductList] = useState([]);
+	const [modelList, setModelList] = useState([]);
+	const [sendTempdata, setSendTempdata] = useState({
+		product_id: "",
+		model_id: "",
+	});
+	const [templateData, setTemplateData] = useState();
+	const [serialPartMapping, setSerialPartMapping] = useState({
+		product_id: "",
+		model_id: "",
+		serial_number: "",
+		employee_id: localStorage.getItem("employee_id"),
+	});
+    const [partLookupData, setPartLookupData] = useState([])
+
+	useQuery("product-list", fetchProductList);
+	function fetchProductList() {
+		axios({
+			method: "get",
+			url: `${baseurl.base_url}/sim/get-product`,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+		})
+			.then((res) => {
+				setProductList(res.data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(err.response.data.message);
+			});
+	}
+
+	function fetchModelList(value) {
+		const data = {
+			product_id: value,
+		};
+		axios({
+			method: "post",
+			url: `${baseurl.base_url}/sim/get-model`,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+			data,
+		})
+			.then((res) => {
+				setModelList(res.data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(err.response.data.message);
+			});
+	}
+
+	function fetchPartTemplate() {
+		axios({
+			method: "post",
+			url: `${baseurl.base_url}/sim/get-part-template`,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+			data: sendTempdata,
+		})
+			.then((res) => {
+				console.log(res);
+				setTemplateData(res.data.data);
+                let array =[];
+                for (const item of res.data.data) {
+                    array.push({id:item.id, serial_number:""})
+                }
+                console.log(array);
+                setPartLookupData(array)
+
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(err.response.data.message);
+			});
+	}
+
+    function sendSerialPartMapping(){
+        console.log(serialPartMapping)
+        const data = serialPartMapping
+        data.part_lookup = partLookupData;
+        console.log(data);
+        
+        axios({
+			method: "post",
+			url: `${baseurl.base_url}/sim/store-serial-part-mapping`,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+            data
+		})
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+
+	return (
+		<div>
+			<div className="home-autocomplete-input-main">
+				<Autocomplete
+					className="autocomp-input"
+					onChange={(event, newValue) => {
+						if (newValue?.id) {
+							fetchModelList(newValue?.id);
+							setSendTempdata({ ...sendTempdata, product_id: newValue.id });
+                            setSerialPartMapping({...serialPartMapping, product_id:newValue.id})
+						}
+					}}
+					disablePortal
+					id="combo-box-demo"
+					getOptionLabel={(option) => `${option.product_name} (${option.product_code})`}
+					options={productList}
+					sx={{ width: 280 }}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							label="Product Name"
+							inputProps={{
+								...params.inputProps,
+							}}
+						/>
+					)}
+				/>
+
+				<Autocomplete
+					className="autocomp-input"
+					onChange={(event, newValue) => {
+						if (newValue?.id) {
+							setSendTempdata({ ...sendTempdata, model_id: newValue.id });
+                            setSerialPartMapping({...serialPartMapping, model_id:newValue.id})
+
+						}
+					}}
+					disablePortal
+					id="combo-box-demo"
+					getOptionLabel={(option) => `${option.model_name} (${option.model_code})`}
+					options={modelList}
+					sx={{ width: 280 }}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							label="Model Name"
+							inputProps={{
+								...params.inputProps,
+							}}
+						/>
+					)}
+				/>
+				<Button
+					size="large"
+					style={{ width: "150px" }}
+					variant="contained"
+					onClick={() => {
+						fetchPartTemplate();
+					}}>
+					Search
+				</Button>
+			</div>
+			<Box sx={{ border: "1px solid rgba(0, 0, 0, 0.12)", borderRadius: "10px", height: "60vh", width: "80%", margin: "30px 0px 0px 0px", padding: "15px" }}>
+				<TextField size="medium" sx={{ width: "300px" }} label="Serial Number" onChange={(e)=>{
+                    setSerialPartMapping({...serialPartMapping, serial_number:e.target.value})
+                }}></TextField>
+				<div style={{ marginTop: "5vh" }}>
+					{templateData?.map((item) => {
+						return (
+							<Box display="flex" gap="40px" alignItems="center" justifyContent="space-between" marginBottom="4vh" padding="0px 20vw 0px 0px">
+								<div>
+									<Typography>Product Name : {item.product_name}</Typography>
+									<Typography>Model Name : {item.model_name} </Typography>
+								</div>
+								<TextField size="small" onChange={(e)=>{
+                                    setPartLookupData([...partLookupData].map(object =>{
+                                        if(item.id == object.id){
+                                            return{
+                                                ...object,
+                                                serial_number:e.target.value
+                                            }
+                                        }
+                                        else return object;
+                                    }))
+
+                                }}></TextField>
+							</Box>
+						);
+					})}
+				</div>
+				<Button sx={{ marginTop: "20px" }} variant="contained" size="large" onClick={()=>{
+                    sendSerialPartMapping()
+                    
+                }}>
+					Send Data
+				</Button>
+			</Box>
+		</div>
+	);
+}
+
+export default Scanning;
