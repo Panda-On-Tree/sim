aed85785ab25abf89afaef97dc249afa563c623f
Aniket Khatri <aksonipat2000@gmail.com>
2023-01-11 14:52:06 +0530

    Update ProductiveView.jsx

--- src/Pages/charts/ProductiveView.jsx
+++ src/Pages/charts/ProductiveView.jsx
@@ -209,6 +209,27 @@ export const ProductiveView = () => {
 			align: "center",
 			verticalAlign: "middle",
 		},
+        markers: {
+            size: 4,
+            colors: undefined,
+            strokeColors: '#fff',
+            strokeWidth: 2,
+            strokeOpacity: 0.9,
+            strokeDashArray: 0,
+            fillOpacity: 1,
+            discrete: [],
+            shape: "circle",
+            radius: 2,
+            offsetX: 0,
+            offsetY: 0,
+            onClick: undefined,
+            onDblClick: undefined,
+            showNullDataPoints: true,
+            hover: {
+              size: undefined,
+              sizeOffset: 3
+            }
+        }
 	};
 	const series = [
 		{
